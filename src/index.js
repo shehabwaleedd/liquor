024d240a0b25d8a537b18b037bef88b6c8147045
Shehab Waleed <shehabwaleedd@gmail.com>
2023-06-24 18:21:50 +0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,42 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import i18next from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import HttpApi from 'i18next-http-backend'
+import LanguageDetector from 'i18next-browser-languagedetector'
+import { BrowserRouter as Router } from 'react-router-dom';
+
+
+
+i18next
+  .use(HttpApi)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    supportedLngs: ['en', 'ar', 'fr', "de"],
+    fallbackLng: 'en',
+    debug: false,
+    // Options for language detector
+    detection: {
+      order: ['path', 'cookie', 'htmlTag'],
+      caches: ['cookie'],
+    },
+    // react: { useSuspense: false },
+    backend: {
+      loadPath: '/assets/locales/{{lng}}/translation.json',
+    },
+  })
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+      <Router>
+        <App />
+      </Router>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/nav/mainNav/MainNav.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './MainNav.css'
+
+const MainNav = () => {
+    return (
+        <nav>
+            
+        </nav>
+    )
+}
+
+export default MainNav
